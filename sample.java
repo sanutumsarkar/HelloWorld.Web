a7a312d53827e3a143f097991d516411ebd6a4bb
sanutumsarkar <84971664+sanutumsarkar@users.noreply.github.com>
2021-12-17 12:40:28 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 // Your First Program
 
-class HelloWorld1 {
+class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World1!"); 
+        System.out.println("Hello, World!"); 
     }
 }
